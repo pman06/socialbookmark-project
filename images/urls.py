a9246604462e20de540a446f5e4d82235e4cc533
paulino007 <paul@paulshoremekun.com>
2020-11-19 16:13:03 -0700

    Added more functionalities such as ajax calls to update image likes and unlikes, ajax calls to load images with continuous scroll, and added bookmarklets to be added in the bookmarkbar for easy image bookmark from another website

--- images/urls.py
+++ images/urls.py
@@ -5,4 +5,7 @@ app_name = "images"
 
 urlpatterns = [
     path('create/', views.image_create_view, name='create'),
+    path('details/<int:id>/<slug:slug>/', views.image_detail, name ='detail'),
+    path('like/', views.image_like_view, name='like'),
+    path('', views.image_list_view, name='list'),
 ]
--- images/views.py
+++ images/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import ImageCreationForm
+from django.http import JsonResponse, HttpResponse
+from django.views.decorators.http import require_POST
 
+from .forms import ImageCreationForm
+from .models import Image
+from bookmarks.common.decorators import ajax_required
 # Create your views here.
 @login_required
 def image_create_view(request):
@@ -25,3 +30,48 @@ def image_create_view(request):
         form = ImageCreationForm(data = request.GET)
 
     return render(request, 'images/image/create.html', {'section':'images', 'form':form})
+
+
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    if image:
+        print('Image found')
+    return render(request, 'images/image/detail.html', {'section':'images', 'image':image})
+
+@ajax_required
+@login_required
+@require_POST
+def image_like_view(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.user_likes.add(request.user)
+            else:
+                image.user_likes.remove(request.user)
+            return JsonResponse({ 'status': 'ok' })
+        except:
+            pass
+    return JsonResponse({'status':'error'})
+
+@login_required
+def image_list_view(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        #If page is not an integer, deliver the first page
+        images = paginator.page(1)
+    except EmptyPage:
+        if request.is_ajax():
+            #If request is ajax and the page is out of range, return empty page
+            return HttpResponse('')
+        #if page is out of range deliver last page of results
+        images = paginator.page(paginator.num_pages)
+    if request.is_ajax():
+        return render(request, 'images/image/list_ajax.html', {'section':'images','images':images})
+    return render(request, 'images/image/list.html', {'section':'images', 'images':images})
